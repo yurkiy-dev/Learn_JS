10a61ca06f85da2963be4108f7d5111f0aafe1e1
yurkiy <prodvinulya@gmail.com>
2020-08-11 23:46:36 +0300

    first DZ

---
+++ newscriptdz.js
@@ -0,0 +1,8 @@
+let numbersOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: 10,
+    movies: '',
+    actors: '',
+    genres:'',
+    private: false
+};
